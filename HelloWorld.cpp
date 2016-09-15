8907bfad6b53ebfd0e07640aa4fce53ddfa4779e
Michael Glass <mglass92@gmail.com>
2016-09-15 12:43:26 +0100

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+// Michael Glass
+// Hello World!
+// 15th Sept 2016
+// Version 1
+// My first program
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	cout << "I am Michael Glass." << endl;
+	cout << "Pleased to meet you." << endl;
+	cout << " :) " << endl;
+	return 0;
+}
