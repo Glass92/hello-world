408a7ed186334991fc6a12a0a279f063c8264d5f
Michael Glass <mglass92@gmail.com>
2016-09-20 11:33:32 +0100

    Add files via upload

---
+++ happybirthday.cpp
@@ -0,0 +1,38 @@
+// Michael Glass
+// Happy Birthday!
+// 15th Sept 2016
+// Version 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "******************" << endl;
+	cout << "*                *" << endl;
+	cout << "* Happy Birthday *" << endl;
+	cout << "*                *" << endl;
+	cout << "******************" << endl;
+	
+	int age;
+	cout << "How old are you today?" << endl;
+	cin >> age;
+	cout << endl;
+
+	if (age >= 17)
+
+	{
+		cout << "You can now learn to drive!" << endl;
+
+	}
+	
+	else
+
+	{
+		cout << "You cannot learn to drive yet, sorry!" << endl;
+
+	}
+	
+	return 0;
+}
+int age;
